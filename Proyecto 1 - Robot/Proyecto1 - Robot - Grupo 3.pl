937f1c99ceb8f1f430ed67d3800ffc78d90605ed
RenixExtreme <ivanrene10@gmail.com>
2021-03-16 23:15:08 -0500

    Update Proyecto

--- Proyecto 1 - Robot/Proyecto1 - Robot - Grupo 3.pl
+++ Proyecto 1 - Robot/Proyecto1 - Robot - Grupo 3.pl
@@ -11,34 +11,17 @@ plan(State1, State, [Action|R], States) :-  go(State1, State2, Action),
                                             not(member(State2, States)),
                                             plan(State2, State, R, [State2|States]).
 
-%%Implementando intercambio y estados
+% Implementando intercambio y estados
 
+go(state(robot(H),en(C1,H1),en(C2,H),pinza(0)), state(robot(H),en(C1,H1),en(C2,H),pinza(C2)), 'Coger la Caja ' + C2 + ' en ' + H).
+go(state(robot(H),en(C1,H),en(C2,H2),pinza(0)), state(robot(H),en(C1,H),en(C2,H2),pinza(C1)), 'Coger la Caja ' + C1 + ' en ' + H).
 
-go(state(robot(H),en(C1,H1),en(C2,H2),pinza(0)), state(robot(H),en(C1,H1),en(C2,H2),pinza(C2)), 'Coger').
-go(state(robot(H),en(C1,H1),en(C2,H2),pinza(0)), state(robot(H),en(C1,H1),en(C2,H2),pinza(C1)), 'Coger').
+go(state(robot(H),en(C1,H),en(C2,H2),pinza(C1)), state(robot(H),en(C1,H),en(C2,H2),pinza(0)), 'Soltar en '+ H).
+go(state(robot(H),en(C1,H1),en(C2,H),pinza(C2)), state(robot(H),en(C1,H1),en(C2,H),pinza(0)), 'Soltar en '+ H).
 
-go(state(robot(H),en(C1,H1),en(C2,H2),pinza(C1)), state(robot(H),en(C1,H1),en(C2,H2),pinza(0)), 'Soltar').
-go(state(robot(H),en(C1,H1),en(C2,H2),pinza(C2)), state(robot(H),en(C1,H1),en(C2,H2),pinza(0)), 'Soltar').
+go(state(robot(h1),en(C1,h1),en(C2,H2),pinza(C1)), state(robot(h2),en(C1,h2),en(C2,H2),pinza(C1)), 'Pasar de h1 a h2').
+go(state(robot(h1),en(C1,H1),en(C2,h1),pinza(C2)), state(robot(h2),en(C1,H1),en(C2,h2),pinza(C2)), 'Pasar de h1 a h2').
+go(state(robot(h2),en(C1,h2),en(C2,H2),pinza(C1)), state(robot(h1),en(C1,h1),en(C2,H2),pinza(C1)), 'Pasar de h2 a h1').
+go(state(robot(h2),en(C1,H1),en(C2,h2),pinza(C2)), state(robot(h1),en(C1,H1),en(C2,h1),pinza(C2)), 'Pasar de h2 a h1').
 
-go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(C1)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(C1)), 'Pasar').
-go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(C2)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(C2)), 'Pasar').
-go(state(robot(h2),en(C1,H1),en(C2,H2),pinza(C1)), state(robot(h1),en(C1,H1),en(C2,H2),pinza(C1)), 'Pasar').
-go(state(robot(h2),en(C1,H1),en(C2,H2),pinza(C2)), state(robot(h1),en(C1,H1),en(C2,H2),pinza(C2)), 'Pasar').
-
-go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), 'Volver').
-go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), 'Volver').
-go(state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), 'Volver').
-go(state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), 'Volver').
-
-
- 
-%go( state(0, L2) , state(3, L2) , 'fill pitcher1' ).
-%go( state(L1, 0) , state(L1, 4) , 'fill pitcher2' ).
-%go( state(L1, L2), state(0, L2) , 'empty pitcher1') :- L1 > 0.
-%go( state(L1, L2), state(L1, 0) , 'empty pitcher2') :- L2 > 0.
-
-%go( state(L1, L2), state(L3, 4), 'pitcher1 to pitcher2') :-  L1 > 0 , L2 < 4 , L2+L1 >= 4 , L3 is L1-(4-L2).
-%go( state(L1, L2), state(0, L4), 'pitcher1 to pitcher2') :-  L1 > 0 , L2 < 4 , L2+L1 < 4  , L4 is L2+L1.
-                                                                                    
-%go( state(L1, L2), state(3, L4), 'pitcher2 to pitcher1') :-  L2 > 0 , L1 < 3 , L1+L2 >= 3 , L4 is L2-(3-L1).
-%go( state(L1, L2), state(L3, 0), 'pitcher2 to pitcher1') :-  L2 > 0 , L1 < 3 , L1+L2 < 3  , L3 is L1+L2.
+go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), 'Pasar de h1 a h2 sin Caja').
