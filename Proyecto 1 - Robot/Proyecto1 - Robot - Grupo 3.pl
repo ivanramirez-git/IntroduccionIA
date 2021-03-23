b5607ff32fde2e6ae966c6289ac1fa3df59ca40a
RenixExtreme <ivanrene10@gmail.com>
2021-03-23 17:36:42 -0500

    Final

--- Proyecto 1 - Robot/Proyecto1 - Robot - Grupo 3.pl
+++ Proyecto 1 - Robot/Proyecto1 - Robot - Grupo 3.pl
@@ -10,8 +10,8 @@ plan(State1, State, [Action|R], States) :-  go(State1, State2, Action),
 
 % Implementando intercambio y estados
 
-go(state(robot(H),en(C1,H1),en(C2,H),pinza(0)), state(robot(H),en(C1,H1),en(C2,H),pinza(C2)), 'Coger la Caja ' + C2 + ' en ' + H).
 go(state(robot(H),en(C1,H),en(C2,H2),pinza(0)), state(robot(H),en(C1,H),en(C2,H2),pinza(C1)), 'Coger la Caja ' + C1 + ' en ' + H).
+go(state(robot(H),en(C1,H1),en(C2,H),pinza(0)), state(robot(H),en(C1,H1),en(C2,H),pinza(C2)), 'Coger la Caja ' + C2 + ' en ' + H).
 
 go(state(robot(H),en(C1,H),en(C2,H2),pinza(C1)), state(robot(H),en(C1,H),en(C2,H2),pinza(0)), 'Soltar en '+ H).
 go(state(robot(H),en(C1,H1),en(C2,H),pinza(C2)), state(robot(H),en(C1,H1),en(C2,H),pinza(0)), 'Soltar en '+ H).
@@ -23,3 +23,4 @@ go(state(robot(h2),en(C1,H1),en(C2,h2),pinza(C2)), state(robot(h1),en(C1,H1),en(
 
 go(state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), 'Pasar de h1 a h2 sin Caja').
 go(state(robot(h2),en(C1,H1),en(C2,H2),pinza(0)), state(robot(h1),en(C1,H1),en(C2,H2),pinza(0)), 'Pasar de h2 a h1 sin Caja').
+
